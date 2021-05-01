7bf261c50a7e3539ce20433db7952b0e05d8602c
Simon2K <simon_sim88@yahoo.com>
2021-05-01 15:16:36 +0800

    Fix error detected by super linter

--- reg_check.php
+++ reg_check.php
@@ -2,25 +2,19 @@
 <html lang="en-us">
 
 <head>
-  
-	<title>MWE | Check</title>
-    
+    <title>MWE | Check</title> 
 </head>
 
 <body>
     <?php
-        
         // echo $_POST['input_nric'];
-        
-        if (file_exists($_POST['input_nric'] . '.txt')){
+        if (file_exists($_POST['input_nric'] . '.txt')) {
             echo '<h3>You are already registered!</h3>';
         }
-        else{
+        else {
             echo '<h3>You have not registered!</h3><br>';
             echo '<a href ="register.html">Register here</a>';
         }
-
     ?>
-   
 </body>
 </html>
